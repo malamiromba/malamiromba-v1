6f10207c6b95e0c671bfaa6f4e7b9961afe78598
Ibrahim Sani <ibraheemsani035@gmail.com>
2026-08-19 14:23:25 +0100

    move redirects from vercel.json to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,32 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "malamiromba.com",
+          },
+        ],
+        destination: "https://techinhausa.org/:path*",
+        permanent: true,
+      },
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "www.malamiromba.com",
+          },
+        ],
+        destination: "https://techinhausa.org/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
